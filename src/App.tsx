bf6589ea80d1bbf41d83a761cbe89b933c9d1882
abdolmajed <abdolmajed3@gmail.com>
2023-04-08 21:29:45 +0330

    start designing form - first components and steps added

---
+++ src/App.tsx
@@ -0,0 +1,29 @@
+import { useMultistepForm } from "./useMultistepForm"
+
+function App() {
+  const { steps, currentStepIndex, step, isFirstStep, back, next } = useMultistepForm([<div>one</div>, <div>two</div>])
+  return (
+    <div className="App" style={{
+      position: "relative",
+      background: "white",
+      border: "1px solid black",
+      padding: "2rem",
+      margin: "1rem",
+      borderRadius: ".5rem",
+      fontFamily: "Arial",
+    }}>
+      <form>
+        <div style={{ position: "absolute", top: ".5rem", right: ".5rem" }}>
+          {currentStepIndex + 1} / {steps.length}
+        </div>
+        {step}
+        <div style={{ marginTop: "1rem", display: "flex", gap: ".5rem", justifyContent: "flex-end" }}>
+          {!isFirstStep && <button onClick={back}>Back</button>}
+          <button onClick={next}>Next</button>
+        </div>
+      </form>
+    </div>
+  )
+}
+
+export default App
