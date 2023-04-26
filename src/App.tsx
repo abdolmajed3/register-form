44b73fe05d2c4f36b8022d0506592b65eaaa5438
abdolmajed <abdolmajed3@gmail.com>
2023-04-26 13:19:51 +0330

    completing form - adding state & verificating form before going next step

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,52 @@
+import { FormEvent, useState } from "react"
 import { AccountForm } from "./accountForm"
 import AddressForm from "./addressForm"
 import { useMultistepForm } from "./useMultistepForm"
 import { UserForm } from "./userForm"
 
+type FormData = {
+  firstName: string
+  lastName: string
+  age: string
+  street: string
+  city: string
+  state: string
+  zip: string
+  email: string
+  password: string
+}
+
+const INITIAL_DATA: FormData = {
+  firstName: "",
+  lastName: "",
+  age: "",
+  street: "",
+  city: "",
+  state: "",
+  zip: "",
+  email: "",
+  password: "",
+}
+
 function App() {
+  const [data, setData] = useState(INITIAL_DATA)
+  function updateFields(fields: Partial<FormData>) {
+    setData(prev => {
+      return { ...prev, ...fields }
+    })
+  }
+
   const { steps, currentStepIndex, step, isFirstStep, isLastStep, back, next } =
-  useMultistepForm([<UserForm />, <AddressForm />, <AccountForm />])
+    useMultistepForm([
+    <UserForm {...data} updateFields={updateFields} />,
+    <AddressForm {...data} updateFields={updateFields} />,
+    <AccountForm {...data} updateFields={updateFields} />])
+
+  function onSubmit(e: FormEvent) {
+    e.preventDefault()
+    if (!isLastStep) return next()
+    alert("done")
+  }
   return (
     <div className="App" style={{
       position: "relative",
@@ -15,8 +56,9 @@ function App() {
       margin: "1rem",
       borderRadius: ".5rem",
       fontFamily: "Arial",
+      maxWidth: "max-content"
     }}>
-      <form>
+      <form onSubmit={onSubmit}>
         <div style={{ position: "absolute", top: ".5rem", right: ".5rem" }}>
           {currentStepIndex + 1} / {steps.length}
         </div>
@@ -27,9 +69,9 @@ function App() {
               Back
             </button>
           )}
-          <button type="button" onClick={next}>
-            {isLastStep ? "finish" : "Next" }
-            </button>
+          <button type="submit">
+            {isLastStep ? "finish" : "Next"}
+          </button>
         </div>
       </form>
     </div>
